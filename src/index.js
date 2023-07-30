fbb976ec4d6b2f0b011694256d49c924b368eefd
Riley Dembo <rmd225@lehigh.edu>
2023-07-29 20:33:26 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//this file is  the bridge between the component you created in the App.js file and the web browser.
+
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
